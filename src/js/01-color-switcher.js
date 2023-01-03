3cb2d93c074e1adec5c370eb1b01c357169c41d0
MelnykY <106350059+MelnykY@users.noreply.github.com>
2023-01-03 21:04:25 +0100

    update

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+let timerId = null;
+
+// startBtn.setAttribute('disabled');
+
+startBtn.addEventListener('click', () => {
+  onClickStartBtn();
+  startBtn.setAttribute('disabled');
+  stopBtn.removeAttribute('disabled');
+});
+
+stopBtn.addEventListener('click', () => {
+  onClickStopBtn();
+  startBtn.removeAttribute('disabled');
+  stopBtn.setAttribute('disabled');
+});
+
+function onClickStartBtn() {
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onClickStopBtn() {
+  clearInterval(timerId);
+};
